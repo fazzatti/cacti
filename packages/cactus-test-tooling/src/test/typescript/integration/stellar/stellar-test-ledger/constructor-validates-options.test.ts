4fd5c6385e7f47d135b7465d60020648b859bbb2
Fabricius Zatti <fazzatti@gmail.com>
2024-04-26 16:22:01 +0000

    test: add integration tests for stellar test ledger

--- packages/cactus-test-tooling/src/test/typescript/integration/stellar/stellar-test-ledger/constructor-validates-options.test.ts
+++ packages/cactus-test-tooling/src/test/typescript/integration/stellar/stellar-test-ledger/constructor-validates-options.test.ts
@@ -1,34 +1,48 @@
-import test, { Test } from "tape-promise/tape";
+import "jest-extended";
 import { Container } from "dockerode";
 import { StellarTestLedger } from "../../../../../main/typescript/public-api";
 import { SupportedImageVersions } from "../../../../../main/typescript/stellar/stellar-test-ledger";
+import axios from "axios";
+import { LogLevelDesc } from "@hyperledger/cactus-common";
 
-test("constructor throws if invalid input is provided", (assert: Test) => {
-  assert.ok(StellarTestLedger);
-  assert.throws(
-    () =>
-      new StellarTestLedger({
-        containerImageVersion: "nope" as unknown as SupportedImageVersions,
-      }),
-  );
-  assert.end();
-});
+describe("StellarTestLEdger", () => {
+  const logLevel: LogLevelDesc = "TRACE";
+  const stellarTestLedger = new StellarTestLedger({ logLevel });
 
-test("constructor does not throw if valid input is provided", (assert: Test) => {
-  assert.ok(StellarTestLedger);
-  assert.doesNotThrow(() => new StellarTestLedger());
-  assert.end();
-});
-
-test("starts/stops/destroys a docker container", async (assert: Test) => {
-  const stellarTestLedger = new StellarTestLedger();
-  test.onFinish(async () => {
+  afterAll(async () => {
     await stellarTestLedger.stop();
     await stellarTestLedger.destroy();
   });
 
-  const container: Container = await stellarTestLedger.start();
-  assert.ok(container);
+  test("constructor throws if invalid input is provided", () => {
+    expect(StellarTestLedger).toBeDefined();
 
-  assert.end();
+    expect(() => {
+      new StellarTestLedger({
+        containerImageVersion: "nope" as unknown as SupportedImageVersions,
+      });
+    }).toThrow();
+  });
+
+  test("constructor does not throw if valid input is provided", async () => {
+    expect(StellarTestLedger).toBeDefined();
+    expect(() => {
+      new StellarTestLedger();
+    }).not.toThrow();
+  });
+
+  test("starts/stops/destroys a valid docker container", async () => {
+    const container: Container = await stellarTestLedger.start();
+    expect(container).toBeDefined();
+
+    const networkConfig = await stellarTestLedger.getNetworkConfiguration();
+    expect(networkConfig).toBeDefined();
+    expect(networkConfig.horizonUrl).toBeDefined();
+    expect(networkConfig.networkPassphrase).toBeDefined();
+    expect(networkConfig.rpcUrl).toBeDefined();
+    expect(networkConfig.friendbotUrl).toBeDefined();
+
+    const horizonResponse = await axios.get(networkConfig.horizonUrl as string);
+    expect(horizonResponse.status).toBe(200);
+  });
 });
